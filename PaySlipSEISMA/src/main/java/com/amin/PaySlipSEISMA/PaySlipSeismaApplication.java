32c62625fd2b4eaa4817f6956ae9159b86ae0e43
Nurul Amin <68992738+Amin0710@users.noreply.github.com>
2022-01-04 03:49:07 +1100

    Initial commit

---
+++ PaySlipSEISMA/src/main/java/com/amin/PaySlipSEISMA/PaySlipSeismaApplication.java
@@ -0,0 +1,13 @@
+package com.amin.PaySlipSEISMA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaySlipSeismaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaySlipSeismaApplication.class, args);
+	}
+
+}
---
+++ PaySlipSEISMA/src/test/java/com/amin/PaySlipSEISMA/PaySlipSeismaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amin.PaySlipSEISMA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaySlipSeismaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
