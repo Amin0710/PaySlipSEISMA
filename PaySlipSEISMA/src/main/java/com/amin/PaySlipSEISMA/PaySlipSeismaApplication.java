c986c51ec393c836299b6b2cb138bace3c37e6a3
Nurul Amin <68992738+Amin0710@users.noreply.github.com>
2022-01-04 06:06:32 +1100

    "Get" setup without csv

--- PaySlipSEISMA/src/main/java/com/amin/PaySlipSEISMA/PaySlipSeismaApplication.java
+++ PaySlipSEISMA/src/main/java/com/amin/PaySlipSEISMA/PaySlipSeismaApplication.java
@@ -1,4 +1,4 @@
-package com.amin.PaySlipSEISMA;
+package com.amin.PaySlipSeisma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- PaySlipSEISMA/src/test/java/com/amin/PaySlipSEISMA/PaySlipSeismaApplicationTests.java
+++ PaySlipSEISMA/src/test/java/com/amin/PaySlipSEISMA/PaySlipSeismaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.amin.PaySlipSEISMA;
+package com.amin.PaySlipSeisma;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
